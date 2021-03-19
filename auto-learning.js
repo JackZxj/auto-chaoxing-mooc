f6f1dd89074bdb9ba839a00fd070ed5a817112c4
jackzhang <x_jackzhang@qq.com>
2021-03-19 16:57:06 +0800

    update for 2021

--- auto-learning.js
+++ auto-learning.js
@@ -5,81 +5,110 @@ class autoScript {
         this.videoSubmit = null;
         this.currentCapter = null;
         this.nextCapter = null;
-        this.runner = null;
+
+        this.stop = true;
     }
 
-    runScript() {
-        this.status = true;
-        var targetIframe = document.getElementById('iframe').contentDocument.getElementsByTagName('iframe')[0].contentDocument;
-        disableAlert(targetIframe.body)
-        this.video = targetIframe.getElementById('video_html5_api');
-        console.log(this.video);
-        this.video.playbackRate = 2.0;
-        this.video.play();
-        var newVideo = false;
-        this.runner = window.setInterval(() => {
-            if (this.video.ended) {
-                console.log('video end');
-                // 视频结束，跳转下一个视频
-                this.currentCapter = document.getElementsByClassName('currents');
-                this.nextCapter = this.currentCapter[1].parentNode.nextElementSibling;
-                if (this.nextCapter) {
-                    this.nextCapter.childNodes[1].childNodes[1].click();
-                } else {
-                    var notLastCapter = this.currentCapter[1].parentNode.parentNode.nextElementSibling;
-                    if (notLastCapter) {
-                        notLastCapter.childNodes[5].childNodes[1].childNodes[1].click();
-                    } else {
-                        console.log('Congratulations on completing the study!');
-                        this.stopScript();
-                        this.finnishSpan();
-                    }
+    async runScript() {
+        this.stop = false;
+        while (!this.stop) {
+            var targetIframe = document.getElementById('iframe').contentDocument.getElementsByTagName('iframe')[0].contentDocument;
+            disableAlert(targetIframe.body)
+            this.video = targetIframe.getElementById('video_html5_api');
+            // console.log(this.video, nowFormat());
+            var homework = document.getElementById("dct2");
+            if (this.video == null) {
+                console.log("no video!", nowFormat());
+                if (homework.classList.length == 1) {
+                    await sleep(10000);
+                    continue;
                 }
-                newVideo = true;
-                return;
-            }
-            if (newVideo) {
-                console.log('new video');
-                targetIframe = document.getElementById('iframe').contentDocument.getElementsByTagName('iframe')[0].contentDocument;
-                disableAlert(targetIframe.body)
-                this.video = targetIframe.getElementById('video_html5_api');
+            } else {
                 this.video.playbackRate = 2.0;
-                newVideo = false;
+                this.video.play();
             }
-            console.log('check pause');
-            if (this.video.paused) {
-                // 获取选项
-                this.videoOptions = targetIframe.getElementsByClassName('ans-videoquiz-opt');
-                this.videoSubmit = targetIframe.getElementsByClassName('ans-videoquiz-submit');
-                console.log(this.videoOptions);
-                // 暂停重播
-                if (this.videoOptions.length == 0) {
-                    console.log('replay video');
-                    this.video.play();
-                } else {
-                    // 选择选项A
-                    console.log('try answer A');
-                    this.videoOptions[0].childNodes[0].childNodes[0].click();
-                    this.videoSubmit[0].click();
-                    // 如果错误，重新选B
-                    if (this.video.paused) {
-                        console.log('try answer B');
-                        this.videoOptions[1].childNodes[0].childNodes[0].click();
-                        this.videoSubmit[0].click();
+            while (true) {
+                if (this.video == null || this.video.ended) {
+                    console.log('video end', nowFormat());
+                    var isCurrentCapterDone = false;
+                    if (homework != null) {
+                        if (homework.classList.length == 1) {
+                            homework.click();
+                        }
+                        await sleep(3000);
+                    }
+                    while (!isCurrentCapterDone) {
+                        if (homework != null) {
+                            this.currentCapter = document.getElementsByClassName('currents')[1];
+                        } else {
+                            this.currentCapter = document.getElementsByClassName('currents')[0];
+                        }
+                        var roundpointStudentClassList = this.currentCapter.getElementsByClassName("roundpointStudent")[0].classList;
+                        console.log(roundpointStudentClassList, nowFormat());
+                        for (var tmp = 0; tmp < roundpointStudentClassList.length; tmp++) {
+                            if (roundpointStudentClassList[tmp] == "blue") {
+                                isCurrentCapterDone = true;
+                                break;
+                            }
+                        }
+                        await sleep(5000);
+                    }
+                    // 视频结束，跳转下一个视频
+                    this.nextCapter = this.currentCapter.parentNode.nextElementSibling;
+                    if (this.nextCapter) {
+                        this.nextCapter.childNodes[1].childNodes[1].click();
+                    } else {
+                        var notLastCapter = this.currentCapter.parentNode.parentNode.nextElementSibling;
+                        if (notLastCapter) {
+                            notLastCapter.childNodes[5].childNodes[1].childNodes[1].click();
+                        } else {
+                            console.log('Congratulations on completing the study!', nowFormat());
+                            this.stopScript();
+                            this.finnishSpan();
+                            return;
+                        }
+                    }
+                    break;
+                }
+                console.log('check pause', nowFormat());
+                if (this.video.paused) {
+                    // 获取选项
+                    this.videoOptions = targetIframe.getElementsByClassName('ans-videoquiz-opt');
+                    this.videoSubmit = targetIframe.getElementsByClassName('ans-videoquiz-submit');
+                    // console.log(this.videoOptions, nowFormat());
+                    // 暂停重播
+                    if (this.videoOptions.length == 0) {
+                        console.log('replay video', nowFormat());
+                        this.video.play();
+                    } else {
+                        var alphabet = ["A", "B", "C", "D", "E", "F", "G"];
+                        for (var i = 0; i < this.videoOptions.length; i++) {
+                            console.log('try answer', alphabet[i], nowFormat(), nowFormat());
+                            this.videoOptions[i].childNodes[0].childNodes[0].click();
+                            this.videoSubmit[0].click();
+                            await sleep(500);
+                            if (!this.video.paused) {
+                                break;
+                            }
+                            console.log('Error', alphabet[i], nowFormat());
+                        }
                     }
                 }
+                await sleep(5000);
             }
-        }, 10 * 1000)
+            await sleep(10000);
+        }
     }
 
     stopScript() {
-        console.log('stop script');
+        console.log('stop script', nowFormat());
         this.video = null;
         this.videoOptions = null;
         this.videoSubmit = null;
         this.currentCapter = null;
         this.nextCapter = null;
-        clearInterval(this.runner);
+
+        this.stop = true;
     }
 
     finnishSpan() {
@@ -96,36 +125,47 @@ class autoScript {
     }
 }
 
+function sleep(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms))
+}
+
+function nowFormat() {
+    var date = new Date();
+    return "    " + date.getHours() + ":" + date.getMinutes() + ":" + date.getSeconds() + ":" + date.getMilliseconds();
+}
+
 function addBtn() {
     var run = new autoScript();
 
     var btnStart = document.createElement('input');
     btnStart.type = 'button';
     btnStart.value = 'start auto script';
-    btnStart.addEventListener('click', () => { run.runScript(); });
     btnStart.style.zIndex = 999;
     btnStart.style.padding = '5px';
     btnStart.style.margin = '10px';
     btnStart.style.position = 'fixed';
     btnStart.style.top = '50px';
-    document.body.appendChild(btnStart);
 
     var btnStop = document.createElement('input');
     btnStop.type = 'button';
     btnStop.value = 'stop auto script';
-    btnStop.addEventListener('click', () => { run.stopScript(); });
     btnStop.style.zIndex = 999;
     btnStop.style.padding = '5px';
     btnStop.style.margin = '10px';
     btnStop.style.position = 'fixed'
     btnStop.style.top = '90px';
+    btnStop.disabled = true;
+
+    btnStart.addEventListener('click', () => { btnStart.disabled = true; btnStop.disabled = false; run.runScript(); });
+    btnStop.addEventListener('click', () => { btnStart.disabled = false; btnStop.disabled = true; run.stopScript(); });
+    document.body.appendChild(btnStart);
     document.body.appendChild(btnStop);
 }
 
 function disableAlert(element) {
     var scriptElement = document.createElement('script');
     scriptElement.innerHTML = 'window.alert = () => { };'
-    scriptElement.lang='javascript'
+    scriptElement.lang = 'javascript'
     element.appendChild(scriptElement);
 }
 
